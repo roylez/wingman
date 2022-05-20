3715a2254b4443c1f6f18ff41c1b3a9c66d0e0fd
roylez <me@roylez.info>
2022-05-20 14:46:00 +1000

    fix: no more debug logging in prod

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,7 @@
 import Config
 
 config :logger, :console,
+  level: config_env() == :prod && :info || :debug,
   format: "$date $time $levelpad$message\n"
 
 config :tesla, adapter: Tesla.Adapter.Hackney
